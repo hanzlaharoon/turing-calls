206cc3e9723252b398824d8f97715c632dd2f3d4
Hanzla Haroon <hanzla.haroon@outlook.com>
2023-07-22 16:50:21 +0500

    fix: use correct auth context in navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,11 +1,11 @@
 'use client'
 
-import { useAuth } from '@/hooks'
+import { useAuthContext } from '@/hooks'
 import ttLogo from '../../app/tt-logo.png'
 import Image from 'next/image'
 
 export function NavBar() {
-  const authState = useAuth()
+  const authState = useAuthContext()
   const {
     state: { status },
     actions: { logout },
@@ -17,7 +17,7 @@ export function NavBar() {
 
   return (
     <>
-      <div className="navbar bg-base-100 border-2 border-trueGray-300">
+      <div className="navbar bg-base-100 border-2 border-trueGray-300 rounded-sm">
         <div className="flex-1">
           <Image
             src={ttLogo}
