10215db13f7d633431cf6f579e6c84785874a737
Hanzla Haroon <hanzla.haroon@outlook.com>
2023-07-23 01:25:59 +0500

    feat: added refresh token handling

--- src/hooks/auth.ts
+++ src/hooks/auth.ts
@@ -1,8 +1,129 @@
-import { User } from "@/types"
+import { LoginResponse } from '@/queries'
+import { User } from '@/types'
+import { createContext, useContext, useEffect, useReducer } from 'react'
 
 interface AuthState {
-    user: User
-    accessToken: string
-    refreshToken: string
+  status: 'loggedIn' | 'loggedOut'
+  user: User
+  accessToken: string
+  refreshToken: string
+  tokenCreatedAt: string
+}
+
+const initialAuthState: AuthState = {
+  status: 'loggedOut',
+  user: {
+    id: '',
+    username: '',
+  },
+  accessToken: '',
+  refreshToken: '',
+  tokenCreatedAt: '',
+}
+
+interface LoginAction {
+  type: 'Login'
+  payload: LoginResponse & Pick<AuthState, 'tokenCreatedAt'>
+}
+
+interface LogoutAction {
+  type: 'Logout'
+}
+
+interface RefreshTokenAction {
+  type: 'RefreshToken'
+  payload: Omit<LoginAction['payload'], 'refreshToken'>
+}
+
+type AuthActions = LoginAction | LogoutAction | RefreshTokenAction
+
+function authReducer(state: AuthState, action: AuthActions): AuthState {
+  switch (action.type) {
+    case 'Login':
+      return {
+        ...state,
+        ...action.payload,
+        status: 'loggedIn',
+      }
+
+    case 'Logout':
+      return { ...initialAuthState }
+
+    case 'RefreshToken':
+      return {
+        ...state,
+        ...action.payload,
+      }
+
+    default:
+      return state
   }
-  
+}
+
+export const useAuth = () => {
+  const [state, dispatch] = useReducer(authReducer, initialAuthState)
+
+  function login(payload: Omit<LoginAction['payload'], 'tokenCreatedAt'>) {
+    const { user, accessToken, refreshToken } = payload
+    const tokenCreatedAt = Date.now().toString()
+    localStorage.setItem('user', JSON.stringify(user))
+    localStorage.setItem('accessToken', accessToken)
+    localStorage.setItem('refreshToken', refreshToken)
+    localStorage.setItem('tokenCreatedAt', tokenCreatedAt)
+    dispatch({ type: 'Login', payload: { ...payload, tokenCreatedAt } })
+  }
+
+  function refreshToken(
+    payload: Omit<RefreshTokenAction['payload'], 'tokenCreatedAt'>
+  ) {
+    const { user, accessToken } = payload
+    const tokenCreatedAt = Date.now().toString()
+    localStorage.setItem('user', JSON.stringify(user))
+    localStorage.setItem('accessToken', accessToken)
+    localStorage.setItem('tokenCreatedAt', tokenCreatedAt)
+    dispatch({ type: 'RefreshToken', payload: { ...payload, tokenCreatedAt } })
+  }
+
+  function logout() {
+    localStorage.removeItem('user')
+    localStorage.removeItem('accessToken')
+    localStorage.removeItem('refreshToken')
+    localStorage.removeItem('tokenCreatedAt')
+    dispatch({ type: 'Logout' })
+  }
+
+  useEffect(() => {
+    const user = localStorage.getItem('user')
+    const accessToken = localStorage.getItem('accessToken')
+    const refreshToken = localStorage.getItem('refreshToken')
+    const tokenCreatedAt = localStorage.getItem('tokenCreatedAt')
+
+    if (user && accessToken && refreshToken && tokenCreatedAt) {
+      const parsedUser = JSON.parse(user)
+
+      login({ user: parsedUser, accessToken, refreshToken })
+    }
+  }, [])
+
+  return {
+    state,
+    actions: {
+      login,
+      logout,
+      refreshToken,
+    },
+  }
+}
+
+const AuthContext = createContext<ReturnType<typeof useAuth>>({
+  state: initialAuthState,
+  actions: {
+    login: () => {},
+    logout: () => {},
+    refreshToken: () => {},
+  },
+})
+
+export const AuthProvider = AuthContext.Provider
+
+export const useAuthContext = () => useContext(AuthContext)
--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,14 +1,68 @@
+import { LoginResponse } from '@/queries'
 import axios from 'axios'
+import dayjs from 'dayjs'
+
+const bufferMinutesStart = 6
+const bufferMinutesEnd = 8
 
 export const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API,
   timeout: 10000,
 })
 
-axiosClient.interceptors.request.use((req) => {
+async function refreshAuthToken(): Promise<
+  Omit<LoginResponse, 'refreshToken'>
+> {
+  const accessToken = localStorage.getItem('accessToken')
+  const result = await axios
+    .post(
+      `${process.env.NEXT_PUBLIC_API}auth/refresh-token`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    )
+    .then((res) => res?.data)
+
+  return result
+}
+
+axiosClient.interceptors.request.use(async (req) => {
   if (req.url !== '/auth/login') {
     const accessToken = localStorage.getItem('accessToken')
-    if (accessToken) req.headers.Authorization = `Bearer ${accessToken}`
+    const tokenCreated = localStorage.getItem('tokenCreatedAt')
+
+    if (accessToken && tokenCreated) {
+      const tokenCreatedAt = parseInt(tokenCreated)
+      const currentTime = dayjs()
+
+      const beforeTokenBuffer = dayjs(tokenCreatedAt).add(
+        bufferMinutesStart,
+        'minute'
+      )
+
+      const afterTokenBuffer = dayjs(tokenCreatedAt).add(
+        bufferMinutesEnd,
+        'minute'
+      )
+
+      const isExpired = currentTime.isAfter(afterTokenBuffer)
+
+      const isTokenNearExpired =
+        currentTime.isAfter(beforeTokenBuffer) &&
+        currentTime.isBefore(afterTokenBuffer)
+
+      if (isTokenNearExpired) {
+        const result = await refreshAuthToken()
+        req.headers.Authorization = `Bearer ${result?.accessToken}`
+      }
+
+      if (!isExpired) {
+        req.headers.Authorization = `Bearer ${accessToken}`
+      }
+    }
   }
 
   return req
